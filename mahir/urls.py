e7c276fa8aff7029b6ac023788da0ea99a88b050
Fahad <mahirajmolfahad3@gmail.com>
2025-12-24 17:58:31 +0600

    fixing a problem

--- mahir/urls.py
+++ mahir/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from tasks import views as task_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include("tasks.urls"))
+    path('', task_views.index, name='index'),  # root URL goes to tasks.index
+    path('tasks/', include("tasks.urls")),
 ]
